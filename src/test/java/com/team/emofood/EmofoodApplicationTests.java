3e18be0b55f9777f8d2d2b87b932a7579c21c61b
cornty8 <cornty8@naver.com>
2022-10-21 19:21:22 +0900

    cornty8

---
+++ src/test/java/com/team/emofood/EmofoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.emofood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmofoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
