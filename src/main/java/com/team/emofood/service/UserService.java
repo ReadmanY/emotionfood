be1319f434ffcf825e631ab90bf52a0887ea6f14
이승연 <50094894+leeseungyeon002@users.noreply.github.com>
2022-10-24 08:22:57 +0900

    delete login

--- src/main/java/com/team/emofood/service/UserService.java
+++ src/main/java/com/team/emofood/service/UserService.java
@@ -1,6 +1,5 @@
 package com.team.emofood.service;
 
-import com.team.emofood.controller.vo.AccountContext;
 import com.team.emofood.dto.User;
 import com.team.emofood.util.PageQueryUtil;
 import com.team.emofood.util.PageResult;
@@ -10,8 +9,5 @@ public interface UserService {
     // 상품 정보 가져오기
     PageResult getUserPage(PageQueryUtil pageUtil);
 
-    public void joinUser(User user);
-
-    public User loadUserByUsername(String userId);
 
 }
